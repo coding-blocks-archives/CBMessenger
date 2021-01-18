250d4183a0780afbb07e55cb1b09e050ab0271bb
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2021-01-18 22:15:26 +0530

    add phone number activity

---
+++ app/src/main/java/com/puldroid/cbmessenger/OtpActivity.kt
@@ -0,0 +1,11 @@
+package com.puldroid.cbmessenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_otp)
+    }
+}
