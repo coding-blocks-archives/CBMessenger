3ddaac8f48962b863a98ccc4fba6192c83f5e928
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2021-01-18 21:46:01 +0530

    initial commit

---
+++ app/src/main/java/com/puldroid/cbmessenger/IntroActivity.kt
@@ -0,0 +1,18 @@
+package com.puldroid.cbmessenger
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_intro.*
+
+class IntroActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_intro)
+
+    findViewById<Button>(R.id.button).setOnClickListener {
+      startActivity(Intent(this, PhoneNoActivity::class.java))
+    }
+  }
+}
---
+++ app/src/main/java/com/puldroid/cbmessenger/PhoneNoActivity.kt
@@ -0,0 +1,11 @@
+package com.puldroid.cbmessenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PhoneNoActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_phone_no)
+  }
+}
