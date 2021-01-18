fa3f596084d0e78abb46cbfd63e312f1b364b87e
Pulkit Aggarwal <aggarwalpulkit596@gmail.com>
2021-01-18 22:16:10 +0530

    add phone number activity

--- app/src/main/java/com/puldroid/cbmessenger/OtpActivity.kt
+++ app/src/main/java/com/puldroid/cbmessenger/OtpActivity.kt
@@ -4,8 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class OtpActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_otp)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_otp)
+  }
 }
--- app/src/main/java/com/puldroid/cbmessenger/PhoneNoActivity.kt
+++ app/src/main/java/com/puldroid/cbmessenger/PhoneNoActivity.kt
@@ -1,11 +1,48 @@
 package com.puldroid.cbmessenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
+import com.hbb20.CountryCodePicker
+import kotlinx.android.synthetic.main.activity_phone_no.*
+
+const val PHONE_NUMBER = "phone"
 
 class PhoneNoActivity : AppCompatActivity() {
+
+  val phoneNoEt: EditText by lazy {
+    findViewById<EditText>(R.id.phoneNumberEt)
+  }
+
+  val button: Button by lazy {
+    findViewById<Button>(R.id.nextBtn)
+  }
+
+  lateinit var countryCode: String
+  lateinit var phoneNumber: String
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_phone_no)
+
+    phoneNoEt.addTextChangedListener { value ->
+      button.isEnabled = !(value.isNullOrEmpty() || value.length < 10)
+    }
+
+    nextBtn.setOnClickListener {
+      checkNumber()
+    }
+  }
+
+  private fun checkNumber() {
+    countryCode = findViewById<CountryCodePicker>(R.id.ccp).selectedCountryCodeWithPlus
+    phoneNumber = countryCode + phoneNoEt.text.toString()
+
+    //Add some validation here
+    startActivity(Intent(this, OtpActivity::class.java).putExtra(PHONE_NUMBER, phoneNumber))
+    finish()
   }
 }
